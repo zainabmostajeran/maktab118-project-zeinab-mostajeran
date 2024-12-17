bd53a61153b521c0c182b7cb18c9c90379d180df
zainabmostajeran <zainab1372m@gmail.com>
2024-12-17 11:11:04 +0330

    add link to sidebarcategory

--- components/shop/Acardeon.tsx
+++ components/shop/Acardeon.tsx
@@ -10,7 +10,9 @@ export function AccordionDemo() {
   return (
     <Accordion type="single" collapsible className="w-full">
       <AccordionItem value="item-1">
-        <AccordionTrigger>پیتزا</AccordionTrigger>
+        <AccordionTrigger>
+          <Link href="/shop/category/pytza">پیتزا</Link>
+        </AccordionTrigger>
         <AccordionContent>
           <Link href="/shop/category/pyza/pytzadw-nfrh">پیتزا دونفره</Link>
         </AccordionContent>
@@ -19,14 +21,26 @@ export function AccordionDemo() {
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-2">
-        <AccordionTrigger>فرنگی</AccordionTrigger>
-        <AccordionContent></AccordionContent>
-        <AccordionContent>ساندویچ</AccordionContent>
+        <AccordionTrigger>
+          <Link href="/shop/category/frngy">فرنگی</Link>
+        </AccordionTrigger>
+        <AccordionContent>
+          <Link href="/shop/category/frngy/mrgh-swkhary">مرغ سوخاری</Link>
+        </AccordionContent>
+        <AccordionContent>
+          <Link href="/shop/category/frngy/sandwych">ساندویچ</Link>
+        </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-3">
-        <AccordionTrigger>پیش غذا</AccordionTrigger>
-        <AccordionContent>سالاد</AccordionContent>
-        <AccordionContent>سیب زمینی</AccordionContent>
+        <AccordionTrigger>
+          <Link href="/shop/category/pysh-ghtha">پیش غذا</Link>
+        </AccordionTrigger>
+        <AccordionContent>
+          <Link href="/shop/category/pysh-ghtha/salad">سالاد</Link>
+        </AccordionContent>
+        <AccordionContent>
+          <Link href="/shop/category/pysh-ghtha/syb-zmyny">سیب زمینی</Link>
+        </AccordionContent>
       </AccordionItem>
     </Accordion>
   );
