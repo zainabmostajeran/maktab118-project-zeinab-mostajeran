e77376a576fa9934f52ba915543c2b623cacc09e
zainabmostajeran <zainab1372m@gmail.com>
2024-12-20 10:09:56 +0330

    add cart  to store and use persist

--- redux/store.ts
+++ redux/store.ts
@@ -1,14 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import { CartReducer } from "./slices/cartSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "@reduxjs/toolkit";
 
-
-export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    cart: CartReducer,
-  },
+const rootReducer = combineReducers({
+  auth: authReducer,
+  cart: CartReducer,
 });
 
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
